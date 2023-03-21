334b78a66d10bb6be7d7c9adeb9b18169c59664d
Patryk Vizauer <vizauerp@gmail.com>
2023-03-21 14:51:16 +0100

    fix some bad code

--- src/Providers/CashBill.php
+++ src/Providers/CashBill.php
@@ -27,17 +27,39 @@ class CashBill extends CashBillSetters
     {
         $parameters['title'] = $this->title;
         $parameters['amount.value'] = sprintf('%.2f', $this->amount);
-        if (isset($this->currency)) $parameters['amount.currencyCode'] = $this->currency;
-        if (isset($this->returnUrl)) $parameters['returnUrl'] = $this->returnUrl;
-        if (isset($this->description)) $parameters['description'] = $this->description;
-        if (isset($this->negativeReturnUrl)) $parameters['negativeReturnUrl'] = $this->negativeReturnUrl;
-        if (isset($this->additionalData)) $parameters['additionalData'] = $this->additionalData;
-        if (isset($this->paymentChannel)) $parameters['paymentChannel'] = $this->paymentChannel;
-        if (isset($this->language)) $parameters['languageCode'] = $this->language;
-        if (isset($this->referer)) $parameters['referer'] = $this->referer;
-        if (isset($this->firstname)) $parameters['personalData.firstName'] = $this->firstname;
-        if (isset($this->surname)) $parameters['personalData.surname'] = $this->surname;
-        if (isset($this->email)) $parameters['personalData.email'] = $this->email;
+        if (isset($this->currency)) {
+            $parameters['amount.currencyCode'] = $this->currency;
+        }
+        if (isset($this->returnUrl)) {
+            $parameters['returnUrl'] = $this->returnUrl;
+        }
+        if (isset($this->description)) {
+            $parameters['description'] = $this->description;
+        }
+        if (isset($this->negativeReturnUrl)) {
+            $parameters['negativeReturnUrl'] = $this->negativeReturnUrl;
+        }
+        if (isset($this->additionalData)) {
+            $parameters['additionalData'] = $this->additionalData;
+        }
+        if (isset($this->paymentChannel)) {
+            $parameters['paymentChannel'] = $this->paymentChannel;
+        }
+        if (isset($this->language)) {
+            $parameters['languageCode'] = $this->language;
+        }
+        if (isset($this->referer)) {
+            $parameters['referer'] = $this->referer;
+        }
+        if (isset($this->firstname)) {
+            $parameters['personalData.firstName'] = $this->firstname;
+        }
+        if (isset($this->surname)) {
+            $parameters['personalData.surname'] = $this->surname;
+        }
+        if (isset($this->email)) {
+            $parameters['personalData.email'] = $this->email;
+        }
 
         $parameters['sign'] = sha1(implode('', $parameters) . $this->shopKey);
 
@@ -48,12 +70,12 @@ class CashBill extends CashBillSetters
             ]
         ], 'POST');
 
-        if($request->getStatusCode() != 200) {
+        if ($request->getStatusCode() != 200) {
             throw new GeneratePaymentException(sprintf('CashBill error: %s', $request->getBody()));
         }
 
         $json = json_decode($request->getBody());
-        if(isset($json->id) && isset($json->redirectUrl)) {
+        if (isset($json->id) && isset($json->redirectUrl)) {
             return new PaymentGeneratedResponse($json->redirectUrl, $json->id);
         }
 
