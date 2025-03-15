96bacb4234510437d130237ae6aa3c81dad21792
Patryk Vizauer <vizauerp@gmail.com>
2025-03-15 10:48:59 +0100

    refactor: refactor hotpay payment classes

--- src/Online/Methods/HotPayPayment.php
+++ src/Online/Methods/HotPayPayment.php
@@ -1,14 +1,7 @@
 <?php
 
-/**
- * Created with love by: Patryk Vizauer (patryqhyper.pl)
- * Date: 16.05.2022 21:37
- * Using: PhpStorm
- */
-
 namespace PatryQHyper\Payments\Online\Methods;
 
-use GuzzleHttp\Exception\TransferException;
 use PatryQHyper\Payments\Exceptions\PaymentException;
 use PatryQHyper\Payments\Online\PaymentAbstract;
 use PatryQHyper\Payments\Online\PaymentGeneratedResponse;
@@ -31,51 +24,52 @@ class HotPayPayment extends PaymentAbstract
 
     public function __construct(string $secret, string $notificationPassword, string $method)
     {
-        if (!in_array($method, [self::METHOD_PAYSAFECARD, self::METHOD_TRANSFER]))
+        if (!in_array($method, [self::METHOD_PAYSAFECARD, self::METHOD_TRANSFER])) {
             throw new PaymentException('invalid method');
+        }
 
         $this->secret = $secret;
         $this->notificationPassword = $notificationPassword;
         $this->method = $method;
     }
 
-    public function setAmount(float $amount)
+    public function setAmount(float $amount): self
     {
         $this->amount = $amount;
         return $this;
     }
 
-    public function setDescription(string $description)
+    public function setDescription(string $description): self
     {
         $this->description = $description;
         return $this;
     }
 
-    public function setRedirectUrl(string $redirectUrl)
+    public function setRedirectUrl(string $redirectUrl): self
     {
         $this->redirectUrl = $redirectUrl;
         return $this;
     }
 
-    public function setOrderId(string $orderId)
+    public function setOrderId(string $orderId): self
     {
         $this->orderId = $orderId;
         return $this;
     }
 
-    public function setEmail(string $email)
+    public function setEmail(string $email): self
     {
         $this->email = $email;
         return $this;
     }
 
-    public function setPersonalData(string $personalData)
+    public function setPersonalData(string $personalData): self
     {
         $this->personalData = $personalData;
         return $this;
     }
 
-    public function generatePayment()
+    public function generatePayment(): PaymentGeneratedResponse
     {
         $params = [
             'KWOTA' => $this->amount,
@@ -91,21 +85,23 @@ class HotPayPayment extends PaymentAbstract
         if (isset($this->personalData)) $params['DANE_OSOBOWE'] = $this->personalData;
         $params['TYP'] = 'INIT';
 
-        $request = $this->doRequest(sprintf('https://%s.hotpay.pl', ($this->method == self::METHOD_TRANSFER ? 'platnosc' : 'psc')), [
+        $request = $this->doRequest(sprintf('https://%s.hotpay.pl', ($this->method === self::METHOD_TRANSFER ? 'platnosc' : 'psc')), [
             'form_params' => $params,
         ], 'POST', false, false);
 
         $json = @json_decode($request->getBody());
-        if (!$json)
+        if (!$json) {
             throw new PaymentException('HotPay error: invalid notification password');
+        }
 
-        if (!$json->STATUS)
-            throw new TransferException('HotPay error: ' . $json->WIADOMOSC);
+        if (!$json->STATUS) {
+            throw new PaymentException('HotPay error: ' . $json->WIADOMOSC);
+        }
 
         return new PaymentGeneratedResponse($json->URL);
     }
 
-    public function generateNotificationHash(array $data)
+    public function generateNotificationHash(array $data): string
     {
         $array = [
             $this->notificationPassword,
@@ -116,6 +112,7 @@ class HotPayPayment extends PaymentAbstract
             $data['SECURE'],
             $data['SEKRET'],
         ];
+
         return hash('sha256', implode(';', $array));
     }
 }
